c1e63399f4984ab7072e60211f650317894523d9
wuxiaojun <wuxiaojun@vipkid.com.cn>
2019-06-17 11:41:14 +0800

    首次提交

---
+++ app/src/main/java/com/wxj/player/MainActivity.java
@@ -0,0 +1,21 @@
+package com.wxj.player;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String					URL		= "http://playback.sayabc.com/test/2704908/7892.mp4";
+
+    private static final String					URL2	= "http://playback.sayabc.com/test/2704908/194956.mp4";
+
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+
+
+	}
+
+}
